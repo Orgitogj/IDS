1a08bea98096f7d8d312e9e7c20125a20af97697
Orgitogj <orgitogjoci10@gmail.com>
2026-09-01 01:13:44 +0200

    Add drift status and detection method enums

---
+++ backend/src/main/java/com/diploma/idsml/entity/DetectionMethod.java
@@ -0,0 +1,6 @@
+package com.diploma.idsml.entity;
+
+public enum DetectionMethod {
+    SUPERVISED_ML,
+    ANOMALY_DETECTION
+}
---
+++ backend/src/main/java/com/diploma/idsml/entity/DriftStatus.java
@@ -0,0 +1,7 @@
+package com.diploma.idsml.entity;
+
+public enum DriftStatus {
+    NORMAL,
+    WARNING,
+    CRITICAL
+}
