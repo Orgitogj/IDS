cf95f837995f92ef2650f341ce22811ad52bcf43
Orgitogj <orgitogjoci10@gmail.com>
2026-09-22 01:11:31 +0200

    feat: use Albanian locale for dates and numbers

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,20 +1,28 @@
 import {
   ApplicationConfig,
+  LOCALE_ID,
   inject,
   provideAppInitializer,
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import localeSq from '@angular/common/locales/sq';
+import { Chart } from 'chart.js';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { AuthService } from './core/services/auth.service';
 
+registerLocaleData(localeSq);
+Chart.defaults.locale = 'sq';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
+    { provide: LOCALE_ID, useValue: 'sq' },
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideCharts(withDefaultRegisterables()),
